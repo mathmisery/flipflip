34c39dea4cdbebf87286f8f059abdf897738cced
mathmisery <mathmisery@gmail.com>
2017-07-24 07:16:04 -0400

    removed reference to express

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,4 @@
 const functions = require('firebase-functions');
-const app = express();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
