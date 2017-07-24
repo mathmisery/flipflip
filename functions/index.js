e37f94babaaec3b0d927bd9970df3239f2cc9c59
mathmisery <mathmisery@gmail.com>
2017-07-24 07:15:22 -0400

    fixed typo in index.js

--- functions/index.js
+++ functions/index.js
@@ -8,9 +8,9 @@ exports.flipfliphi = functions.https.onRequest((request, response) => {
     response.send("Hello this is the Flip! Flip! game!");
 });
 
-exports.flipflipadd = functions.https.onRequest((request, response)) => {
+exports.flipflipadd = functions.https.onRequest((request, response) => {
     var val1 = request.query.val1;
     var val2 = request.query.val2;
     var result = parseInt(val1) + parseInt(val2);
     response.send(val1 + " + " + val2 + " = " + result.toString());
-}
+});
